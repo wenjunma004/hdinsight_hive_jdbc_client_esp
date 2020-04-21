9b0f17f19c006b839da4d13105753951407dcf18
Wenjun Ma <wenjm@microsoft.com>
2020-04-21 10:00:25 -0700

    debug

--- src/main/java/org/apache/hive/jdbc/Utils.java
+++ src/main/java/org/apache/hive/jdbc/Utils.java
@@ -18,11 +18,9 @@
 
 package org.apache.hive.jdbc;
 
-import java.net.InetAddress;
 import java.net.URI;
 import java.net.UnknownHostException;
 import java.sql.SQLException;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
